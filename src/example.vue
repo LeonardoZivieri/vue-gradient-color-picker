0c6b11a8aac25df0ee14ecf0fac372146972323c
Leonardo Zivieri <leonardozivieri150@gmail.com>
2020-07-21 11:25:30 -0300

    Moved testing page to example component

--- src/example.vue
+++ src/example.vue
@@ -1,11 +1,33 @@
 <template lang="html">
 	<div>
-		ops
+		<div v-for="( color, index ) in colors" :key="index">
+			<div>
+				{{ colors[index] }}
+			</div>
+			<gradient-color-picker v-model="colors[index]"></gradient-color-picker>
+		</div>
 	</div>
 </template>
 
 <script>
+import GradientColorPicker from './gradient-color-picker.vue';
+
 export default {
+	components: {
+		GradientColorPicker,
+	},
+	data() {
+		return {
+			colors: [
+				'linear-gradient(#e66465, #9198e5)',
+				'linear-gradient(0.25turn, #3f87a6, #ebf8e1, #f69d3c)',
+				'linear-gradient(to left, #333, #333 50%, #eee 75%, #333 75%)',
+				'linear-gradient(217deg, rgba(255,0,0,.8), rgba(255,0,0,0) 70.71%)',
+				'linear-gradient(127deg, rgba(0,255,0,.8), rgba(0,255,0,0) 70.71%)',
+				'linear-gradient(336deg, rgba(0,0,255,.8), rgba(0,0,255,0) 70.71%)',
+			],
+		};
+	},
 };
 </script>
 
