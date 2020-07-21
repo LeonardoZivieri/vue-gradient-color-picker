f48ac466fc43f61af7c86922c319828c945038d1
Leonardo Zivieri <leonardozivieri150@gmail.com>
2020-07-21 11:23:10 -0300

    Value input process

--- src/gradient-color-picker.vue
+++ src/gradient-color-picker.vue
@@ -48,6 +48,7 @@
 
 <script>
 export default {
+	props: ['value'],
 	data() {
 		return {
 			initialColor: '#000000',
@@ -59,7 +60,13 @@ export default {
 				},
 			],
 			movingColor: null,
+			direction: 0,
 		};
+	},
+	watch: {
+		value(newValue) {
+			this.registerNewValue(newValue);
+		},
 	},
 	computed: {
 		allColors() {
@@ -130,6 +137,131 @@ export default {
 				this.movingColor.percent = Math.max(1, Math.min(percent, 99));
 			}
 		},
+		registerNewValue(newValue) {
+			let value = newValue.trim().replace(/^linear-gradient\(/, '').replace(/\)$/, '');
+
+			const directionPattern = /^((to left|to right|to top|to bottom|[0-9.]+turn|[0-9.]+deg), *)(.*)?/;
+			const directionPatternResult = directionPattern.exec(value);
+			if (!directionPatternResult) {
+				this.direction = 0;
+			} else {
+				// Remove the direction from the gradient string
+				value = value.substring(directionPatternResult[2].length + 1).trim();
+
+				if (directionPatternResult[2].includes('deg')) {
+					this.direction = parseFloat(directionPatternResult[2].replace(/[^0-9.]/, ''));
+				}
+				if (directionPatternResult[2].includes('turn')) {
+					this.direction = 360 * parseFloat(directionPatternResult[2].replace(/[^0-9.]/, ''));
+				} else {
+					const directions = {
+						right: 0,
+						top: 90,
+						left: 180,
+						bottom: 270,
+					};
+					this.direction = directions[directionPatternResult[2].replace('to ', '')] || 0;
+				}
+			}
+
+			if (!this.direction || Number.isNaN(this.direction)) {
+				this.direction = 0;
+			}
+
+			const colors = [];
+			if (directionPatternResult && directionPatternResult[3]) {
+				const colorPattern = /((#[0-9a-fA-F]{3,8}|rgba?\(([0-9.]+(, *)?)*\))\s*([0-9.]+%)?\s*(,\s*)?)/;
+				let colorPatternResult;
+				let color;
+				while (colorPattern.test(value)) {
+					colorPatternResult = colorPattern.exec(value);
+					color = colorPatternResult[2];
+
+					if (color.includes('rgb')) {
+						color = color.replace(', ', ',');
+						color = color.replace(/^rgba?\((.*)\)$/, '$1');
+						color = color.split(',').map(parseFloat);
+						color = this.rgbToHex(...color);
+					}
+					colors.push({
+						color,
+						percent: colorPatternResult[5] ? parseFloat(colorPatternResult[5].replace(/[^0-9.]/, '')) : null,
+					});
+					value = value.substr(colorPatternResult[2].length + 2).trim();
+				}
+				this.colors = colors;
+				if (this.colors[0]) {
+					if (this.colors[0].percent) {
+						this.colors.splice(0, 0, {
+							color: this.colors[0].color,
+							percent: 0,
+						});
+					} else {
+						this.colors[0].percent = 0;
+					}
+				}
+				if (this.colors[this.colors.length - 1]) {
+					if (this.colors[this.colors.length - 1].percent) {
+						this.colors.splice(this.colors.length - 1, 0, {
+							color: this.colors[this.colors.length - 1].color,
+							percent: 100,
+						});
+					} else {
+						this.colors[this.colors.length - 1].percent = 100;
+					}
+				}
+				this.colors.forEach((c, index) => {
+					if (c.percent !== null) {
+						let diference;
+						let nextPercent;
+						let nullPercentsQuantity = 0;
+
+						for (let i = index + 1; i < this.colors.length; i += 1) {
+							if (this.colors[i].percent === null) {
+								nullPercentsQuantity += 1;
+							} else {
+								nextPercent = this.colors[i].percent;
+								break;
+							}
+						}
+
+						if (nullPercentsQuantity > 0 && nextPercent) {
+							diference = nextPercent - c.percent;
+							diference /= nullPercentsQuantity + 1;
+							for (let i = 1; i <= nullPercentsQuantity; i += 1) {
+								this.colors[i].percent = c.percent + (diference * (1 + i - nullPercentsQuantity));
+							}
+						}
+					}
+				});
+				this.colors = this.colors.sort((c1, c2) => c1.percent - c2.percent);
+				this.initialColor = this.colors.shift().color;
+				this.finalColor = this.colors.pop().color;
+			}
+
+			this.$forceUpdate();
+		},
+
+		hexToRgb(hex) {
+			const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+			return result ? {
+				r: parseInt(result[1], 16),
+				g: parseInt(result[2], 16),
+				b: parseInt(result[3], 16),
+			} : null;
+		},
+		componentToHex(c) {
+			const hex = c.toString(16);
+			return hex.length === 1 ? `0${hex}` : hex;
+		},
+		rgbToHex(r, g, b) {
+			return `#${this.componentToHex(r)}${this.componentToHex(g)}${this.componentToHex(b)}`;
+		},
+	},
+	mounted() {
+		if (this.value) {
+			this.registerNewValue(this.value);
+		}
 	},
 };
 </script>
